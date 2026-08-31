686b5d060b6c7c0eb3139ea0914b3f5624aaee11
CODER <CODER@A26-PC4>
2026-08-31 16:20:37 +0300

    Add project files.

---
+++ 01_IntroToC++/FileName.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	//cout << "Hello world\n";
+	//cout << "Hello world\n";
+	//cout << "Hello world\n";
+	//cout << "Hello world\n";
+	//cout << "Hello world\n";
+	//cout << 'f'; //only one symbol
+
+	//task1
+
+	cout << "I";
+	cout << "\n\tlove";
+	cout << "\n\t\tyou";
+	cout << "\n\t\t\tC++!\n";
+
+	//task2
+
+	cout << "\t...:::RESUME:::...\n";
+	cout << "Name:\t\t\tIvan\n";
+	cout << "Surname:\t\tIvanov\n";
+	cout << "Last name:\t\tIvanovich\n";
+	cout << "\n";
+	cout << "Date of birth:\t\t22.06.06\n";
+	cout << "City:\t\t\tRivne\n";
+	cout << "Age:\t\t\t17\n";
+	cout << "\n";
+	cout << "Hobby:\t\t\tC++\n";
+	cout << "::..........................::";
+	
+}
