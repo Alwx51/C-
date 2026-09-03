86daafb62e01c501b33daec9b0f660a4b901d18e
Oleksiy <alex13alexeyev@gmail.com>
2026-09-03 19:12:58 +0200

    homework 3

---
+++ 03_hw/03_hw.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //task1
+    //float a, b, c, d;
+    //cout << "Enter 1 side: " << endl;cin >> a;
+    //cout << "Enter 2 side: " << endl;cin >> b;
+    //cout << "Enter 3 side: " << endl;cin >> c;
+    //cout << "Enter 4 side: " << endl;cin >> d;
+    //if (a == b && b == c && c == d) {
+    //    cout << "Its square" << endl;
+    //}
+    //else {
+    //    cout << "Its not square" << endl;
+    //}
+    //task2
+    //int age;
+
+    //cout << "Enter child's age: " << endl;
+    //cin >> age;
+
+    //if (age < 0) {
+    //    cout << "Wrong age" << endl;
+    //}
+    //else if (age < 7) {
+    //    cout << "Kindergarten" << endl;
+    //}
+    //else if (age < 16) {
+    //    cout << "School" << endl;
+    //}
+    //else {
+    //    cout << "College" << endl;
+    //}
+    //task3
+    //int a, b;
+    //cout << "Enter 1 number: " << endl;cin >> a;
+    //cout << "Enter 2 number: " << endl;cin >> b;
+    //if (a < 0 && b < 0) {
+    //    cout << "two of them are negative" << endl;
+    //}
+    //else {
+    //    cout << "one number is positive" << endl;
+    //}
+    //task4
+    //int num;
+    //cout << "Enter number: " << endl;cin >> num;
+    //if (num > 1 && num < 6) {
+    //    cout << "[2, 5]" << endl;
+    //}
+    //else if (num > -2 && num < 2) {
+    //    cout << "[-1, 1]" << endl;
+    //}
+    //else {
+    //    cout << "does not belong to any" << endl;
+    //}
+    //task5
+    //float a, b, c;
+    //cout << "Enter 1 number: " << endl;cin >> a;
+    //cout << "Enter 2 number: " << endl;cin >> b;
+    //cout << "Enter 3 number: " << endl;cin >> c;
+    //if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a)) {
+    //    cout << "Two of them are equal to each other" << endl;
+    //}
+    //else {
+    //    cout << "They are not equal to each other" << endl;
+    //}
+    //task6
+    //int a, b, c;
+    //cout << "Enter 1 number: " << endl;cin >> a;
+    //cout << "Enter 2 number: " << endl;cin >> b;
+    //cout << "Enter 3 number: " << endl;cin >> c;
+    //if (a % 2 == 1 && b % 2 == 1 && c % 2 == 1) {
+    //    cout << "All the numbers are odd" << endl;
+    //}
+    //else {
+    //    cout << "There are even numbers" << endl;
+    //}
+    //Додаткова оцінка
+    //task7
+    //int a, b;
+    //cout << "Enter 1 number: " << endl;cin >> a;   
+    //cout << "Enter 2 number: " << endl;cin >> b;   
+    //if (a != b) {
+    //    int temp = a;
+    //    a = b;
+    //    b = temp;
+    //}
+    //cout << "a = " << a << endl;
+    //cout << "b = " << b << endl;
+    //task8
+
+}
+
