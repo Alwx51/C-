6622f0de8d07513430a5717f9f21a9eb698ffb2c
Oleksiy <alex13alexeyev@gmail.com>
2026-09-21 21:02:13 +0200

    update homework

--- 09_homework/09_homework.cpp
+++ 09_homework/09_homework.cpp
@@ -17,7 +17,7 @@ double Max(double a, double b)
 	return (a > b) ? a : b;
 
 }
-int Max(int a, int b,int c)
+int Max(int a, int b, int c)
 {
 	if (a > b && a > c)
 		return a;
@@ -27,7 +27,7 @@ int Max(int a, int b,int c)
 		return c;
 
 }
-float Max(float a, float b,float c)
+float Max(float a, float b, float c)
 {
 	if (a > b && a > c)
 		return a;
@@ -36,7 +36,7 @@ float Max(float a, float b,float c)
 	else
 		return c;
 }
-double Max(double a, double b,double c)
+double Max(double a, double b, double c)
 {
 	if (a > b && a > c)
 		return a;
@@ -127,6 +127,133 @@ void ShowMatrix1(int arr[][5], int rows, int cols)
 		cout << endl;
 	}
 }
+void InitMatrix1(double arr[][5], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 90 + 10;
+		}
+	}
+}
+void ShowMatrix1(double arr[][5], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void InitMatrix1(char arr[][5], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 90 + 10;
+		}
+	}
+}
+void ShowMatrix1(char arr[][5], int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void maxDiagonal(int arr[][5], int rows)
+{
+	int max = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (max < arr[i][i])
+		{
+			max = arr[i][i];
+		}
+	}
+
+	cout << "max = " << max << endl;
+}
+void maxDiagonal(double arr[][5], int rows)
+{
+	double max = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (max < arr[i][i])
+		{
+			max = arr[i][i];
+		}
+	}
+
+	cout << "max = " << max << endl;
+}
+void maxDiagonal(char arr[][5], int rows)
+{
+	char max = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (max < arr[i][i])
+		{
+			max = arr[i][i];
+		}
+	}
+
+	cout << "max = " << max << endl;
+}
+void minDiagonal(int arr[][5], int rows)
+{
+	int min = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (min > arr[i][i])
+		{
+			min = arr[i][i];
+		}
+	}
+
+	cout << "min = " << min << endl;
+}
+void minDiagonal(double arr[][5], int rows)
+{
+	double min = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (min > arr[i][i])
+		{
+			min = arr[i][i];
+		}
+	}
+
+	cout << "min = " << min << endl;
+}
+void minDiagonal(char arr[][5], int rows)
+{
+	char min = arr[0][0];
+
+	for (int i = 1; i < rows; i++)
+	{
+		if (min > arr[i][i])
+		{
+			min = arr[i][i];
+		}
+	}
+
+	cout << "min = " << min << endl;
+}
 
 template<typename T_arr>
 void showAverage(T_arr arr[], int size)
@@ -137,10 +264,10 @@ void showAverage(T_arr arr[], int size)
 	{
 		summa += arr[i];
 		count += 1;
-		
+
 	}
-	cout << "Seredne aryphmetichne = " <<(double) summa / count << endl;
-	
+	cout << "Seredne aryphmetichne = " << (double)summa / count << endl;
+
 }
 template<typename T_arr>
 void showMax(T_arr arr[], int size)
@@ -148,14 +275,14 @@ void showMax(T_arr arr[], int size)
 	T_arr max = arr[0];
 	for (int i = 0; i < size; i++)
 	{
-		if (max<arr[i])
+		if (max < arr[i])
 		{
 			max = arr[i];
 		}
-		
+
 	}
-	cout << "Max number = " <<max << endl;
-	
+	cout << "Max number = " << max << endl;
+
 }
 template<typename T_arr>
 void showMax1(T_arr arr[][5], int rows, int cols)
@@ -170,11 +297,11 @@ void showMax1(T_arr arr[][5], int rows, int cols)
 				max = arr[i][j];
 			}
 		}
-		
-		
+
+
 	}
-	cout << "Max number = " <<max << endl;
-	
+	cout << "Max number = " << max << endl;
+
 }
 
 
@@ -182,19 +309,19 @@ void showMax1(T_arr arr[][5], int rows, int cols)
 int main()
 {
 	srand(time(0));
-    //task1
+	//task1
 	cout << "Max number: " << Max(5, 8) << endl;
 	cout << "Max number: " << Max(3.33, 1.22) << endl;
 	cout << "Max number: " << Max(3.34, 2.5) << endl;
-	cout << "Max number: " << Max(5, 8,32) << endl;
-	cout << "Max number: " << Max(3.33, 1.22,7.3) << endl;
-	cout << "Max number: " << Max(3.34, 2.5,4.6) << endl;
+	cout << "Max number: " << Max(5, 8, 32) << endl;
+	cout << "Max number: " << Max(3.33, 1.22, 7.3) << endl;
+	cout << "Max number: " << Max(3.34, 2.5, 4.6) << endl;
 	cout << "Min number: " << Min(5, 8) << endl;
 	cout << "Min number: " << Min(3.33, 1.22) << endl;
 	cout << "Min number: " << Min(3.34, 2.5) << endl;
-	cout << "Min number: " << Min(5, 8,32) << endl;
-	cout << "Min number: " << Min(3.33, 1.22,7.3) << endl;
-	cout << "Min number: " << Min(3.34, 2.5,4.6) << endl;
+	cout << "Min number: " << Min(5, 8, 32) << endl;
+	cout << "Min number: " << Min(3.33, 1.22, 7.3) << endl;
+	cout << "Min number: " << Min(3.34, 2.5, 4.6) << endl;
 	//task2
 	const int size = 10;
 	int arr[size];
@@ -204,14 +331,16 @@ int main()
 	//task3
 	showMax(arr, size);
 	//task3 2
-	const int rows= 5;
-	const int cols= 5;
+	const int rows = 5;
+	const int cols = 5;
 	int arr1[rows][cols] = {};
 	InitMatrix1(arr1, rows, cols);
 	ShowMatrix1(arr1, rows, cols);
-	showMax1(arr1, rows,cols);
+	showMax1(arr1, rows, cols);
+	//task4
+	maxDiagonal(arr1, rows);
+	minDiagonal(arr1, rows);
 
 
 
 }
-
